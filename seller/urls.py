131c8a461fb3230f7ec51c08eee297e3bc7ee7b8
avdeevaa <tess-avdeeva@seznam.cz>
2024-04-16 09:31:26 +0200

    2 _ CRUD for supplier + django filter

--- seller/urls.py
+++ seller/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
 
-# from modules.apps import ModulesConfig
-# from modules.views import ModuleUpdateAPIview, ModuleListAPIview, ModuleDestroyAPIview, ModuleRetrieveAPIview, ModuleCreateAPIview
-#
-# app_name = ModulesConfig.name
-#
-# urlpatterns = [
-#     path('module/create/', ModuleCreateAPIview.as_view(), name='create_module'),
-#     path('modules/', ModuleListAPIview.as_view(), name='all_modules'),
-#     path('module/<int:pk>/', ModuleRetrieveAPIview.as_view(), name='one_module'),
-#     path('module/update/<int:pk>/', ModuleUpdateAPIview.as_view(), name='update_module'),
-#     path('module/delete/<int:pk>/', ModuleDestroyAPIview.as_view(), name='delete_module'),
-# ]
+from seller.apps import SellerConfig
+from seller.views import NetworkNodeListAPIView, SupplierDestroyAPIView, SupplierRetrieveAPIView, SupplierCreateAPIView, \
+    SupplierListAPIView, NetworkNodeUpdateAPIView, SupplierUpdateAPIView
+
+app_name = SellerConfig.name
+
+urlpatterns = [
+    path("suppliers/", SupplierListAPIView.as_view(), name="suppliers-all"),
+    path("suppliers/create/", SupplierCreateAPIView.as_view(), name="suppliers-create"),
+    path("suppliers/<int:pk>/", SupplierRetrieveAPIView.as_view(), name="suppliers-retrieve"),
+    path("suppliers/update/<int:pk>/", SupplierUpdateAPIView.as_view(), name="suppliers-update"),
+    path("suppliers/delete/<int:pk>/", SupplierDestroyAPIView.as_view(), name="suppliers-delete"),
+
+    path("networknode/", NetworkNodeListAPIView.as_view(), name='all-networknode'),
+    path("networknode/update/", NetworkNodeUpdateAPIView.as_view(), name="networknode-update"),
+]
--- seller/views.py
+++ seller/views.py
@@ -1,3 +1,64 @@
 from django.shortcuts import render
+from rest_framework import generics, filters
 
-# Create your views here.
+from seller.models import Supplier, NetworkNode
+from seller.serializers import SupplierSerializer, NetworkNodeSerializer
+
+
+class SupplierCreateAPIView(generics.CreateAPIView):
+    """ Создаем поставщика """
+    serializer_class = SupplierSerializer
+
+
+class SupplierListAPIView(generics.ListAPIView):
+    """ Просматриваем всех поставщиков и настраиваем фильтр по стране"""
+    serializer_class = SupplierSerializer
+    queryset = Supplier.objects.all()
+    filter_backends = [filters.SearchFilter, filters.DjangoFilterBackend]
+    search_fields = ['country']
+    filterset_fields = ['country']
+
+
+class SupplierRetrieveAPIView(generics.RetrieveAPIView):
+    """ Просматриваем одного поставщика """
+    serializer_class = SupplierSerializer
+    queryset = Supplier.objects.all()
+
+
+class SupplierUpdateAPIView(generics.UpdateAPIView):
+    """ Редактируем поставщика """
+    serializer_class = SupplierSerializer
+    queryset = Supplier.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        """Запрещаем обновление поля задолженность"""
+        if 'debt' in request.data:
+            del request.data['debt_to_supplier']
+        return super().update(request, *args, **kwargs)
+
+
+class SupplierDestroyAPIView(generics.DestroyAPIView):
+    """ Удаляем поставшика """
+    serializer_class = SupplierSerializer
+    queryset = Supplier.objects.all()
+
+
+class NetworkNodeListAPIView(generics.ListAPIView):
+    """ Просматриваем все звенья сети и настраиваем фильтр по стране"""
+    serializer_class = NetworkNodeSerializer
+    queryset = NetworkNode.objects.all()
+    filter_backends = [filters.SearchFilter, filters.DjangoFilterBackend]
+    search_fields = ['contacts_country']
+    filterset_fields = ['contacts_country']
+
+
+class NetworkNodeUpdateAPIView(generics.UpdateAPIView):
+    """ Редактируем звенья сети """
+    serializer_class = NetworkNodeSerializer
+    queryset = NetworkNode.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        """Запрещаем обновление поля задолженность"""
+        if 'debt' in request.data:
+            del request.data['debt_to_supplier']
+        return super().update(request, *args, **kwargs)
